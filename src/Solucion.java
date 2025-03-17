ca1c56a291e01444edc2e0eb0066a3b4a46d0ce4
Ciara244 <martinsanchezciara244@gmail.com>
2025-03-17 19:18:25 +0000

    Commit rama, añadir javaDoc

--- src/Solucion.java
+++ src/Solucion.java
@@ -1,2 +1,30 @@
+import java.lang.StringBuilder;
+
+/** 
+ * @author(Ciara Martin);
+ */
 public class Solucion {
+
+    /**
+     * 
+     * @param s
+     * @return String 
+     */
+    static String toCamelCase(String s){
+        String[] lista;
+        StringBuilder camelCase = new StringBuilder();
+        if(s.contains("-")){
+            lista = s.split("-");
+            for(String palabras: lista){
+                camelCase.append(palabras);
+            }
+        }else if(s.contains("_")){
+            lista = s.split("_");
+            for(String palabras: lista){
+                camelCase.append(palabras);
+            }
+        }
+        return camelCase.toString();
+    }
+    
 }
