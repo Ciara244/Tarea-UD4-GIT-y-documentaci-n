bd230f30f9e4a0d0e029ed2624fd2a0f2974fcfc
Ciara244 <martinsanchezciara244@gmail.com>
2025-03-17 19:18:25 +0000

    3º commit, codigo mejorado

--- src/Solucion.java
+++ src/Solucion.java
@@ -1,2 +1,20 @@
+import java.lang.StringBuilder;
 public class Solucion {
+
+    static String toCamelCase(String s){
+        String[] lista;
+        StringBuilder camelCase = new StringBuilder();
+        if(s.contains("-")){
+            lista = s.split("-");
+            for(String palabras: lista){
+                camelCase.append(palabras);
+            }
+        }else if(s.contains("_")){
+            lista = s.split("_");
+            for(String palabras: lista){
+                camelCase.append(palabras);
+            }
+        }
+        return camelCase.toString();
+    }
 }
